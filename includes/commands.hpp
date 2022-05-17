42b68bc2e0c73067a9e1507875806a84c3170724
Elias AACHACH <elaachac@e2r7p16.clusters.42paris.fr>
2022-05-17 11:59:58 +0200

    push before startings commands

---
+++ includes/commands.hpp
@@ -0,0 +1,13 @@
+#ifndef		COMMANDS_HPP
+# define	COMMANDS_HPP
+
+# include "config.hpp"
+# include "user.hpp"
+# include "tcpServer.hpp"
+
+void	cmd_user ( IrcServer *server, user *user, std::string & args )
+{
+	
+}
+
+#endif
