36463b4108dfa97195d490e53da2a844266ba2d7
Elias AACHACH <elaachac@e3r8p4.clusters.42paris.fr>
2022-05-19 18:19:05 +0200

    join wip, heap use after free, 2 join version : current is from lucrece

--- srcs/channels.cpp
+++ srcs/channels.cpp
@@ -6,7 +6,7 @@
 /*   By: elaachac <elaachac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 16:31:24 by elaachac          #+#    #+#             */
-/*   Updated: 2022/05/19 16:41:19 by elaachac         ###   ########.fr       */
+/*   Updated: 2022/05/19 18:18:02 by elaachac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,14 +67,30 @@ void		channels::setOper(user *oper)
 	this->_oper = oper;
 }
 
-void		channels::addUser(user newUser)
+
+void		channels::addUser(user &newUser)
 {
-	this->_currentUsers.insert(std::make_pair(newUser.getSdUser(), &newUser));
-	std::map<int, user*>::const_iterator it;
-	//display all users
-	// std::map<int, user*>::const_iterator user_it = this->getUsers().begin();
-	for (it = this->getUsers().begin(); it != this->getUsers().end(); it++)
-        {
-            std::cout << "DISPLAY USERS:" << it->first << std::endl;
-        }
+	user*	tmp = new user(newUser);
+	this->getUsers().insert(std::make_pair(newUser.getSdUser(), tmp));
+	std::cout << newUser.getNickName() << " has appeared " << std::endl;
+	std::map<int, user*>::iterator it;
+	for ( it = this->getUsers().begin(); it != this->getUsers().end(); it++)
+        std::cout << "USERS : " << it->first;
 }
+
+// void		channels::addUser(user newUser)
+// {
+// 	// user userToAdd = new user(newUser);
+// 	this->_currentUsers.insert(std::make_pair(newUser.getSdUser(), &newUser));
+// 	std::map<int, user*>::const_iterator it;
+// 	int i = 0;
+// 	//display all users
+// 	// std::map<int, user*>::const_iterator user_it = this->getUsers().begin();
+// 	// for (it = this->getUsers().begin(); it != this->getUsers().end(); it++)
+// 	for(it = this->getUsers().begin(), i = this->getUsers().size(); i != 0; i--)
+//         {
+// 			std::cout << "enter: ";
+//             std::cout << "DISPLAY USERS:" << it->first << std::endl;
+// 			// it++;
+//         }
+// }
