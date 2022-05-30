79ae66f63d7770c5f884cb4270bea94711553f09
Clara BOUCHER <clbouche@e3r7p11.clusters.42paris.fr>
2022-05-30 14:02:01 +0200

    split with space AND newline

--- obj/tcpServer.d
+++
@@ -1,6 +0,0 @@
-obj/tcpServer.o: srcs/tcpServer.cpp srcs/../includes/tcpServer.hpp \
-  includes/config.hpp includes/user.hpp
-srcs/tcpServer.cpp:
-srcs/../includes/tcpServer.hpp:
-includes/config.hpp:
-includes/user.hpp:
--- srcs/commands.cpp
+++ srcs/commands.cpp
@@ -6,7 +6,7 @@
 /*   By: clbouche <clbouche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 11:25:28 by clbouche          #+#    #+#             */
-/*   Updated: 2022/05/19 17:45:09 by clbouche         ###   ########.fr       */
+/*   Updated: 2022/05/30 13:47:18 by clbouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,37 @@ void    cmd_NULL( IrcServer *serv, user	currentUser, std::string & args )
 	std::cout << "cmd not found" << std::endl;
 }
 
+/**
+ * @brief Permet de delimiter la commande (pour l'envoyer a la bonne fonction)
+ * des arguments, stocker dans une meme string pour definir le parsing selon 
+ * la commande.
+ * 
+ * @param args L'integralite des arguments envoye par le user sans parsing
+ * @param IRC Le serveur
+ * @param currentUser L'utilisateur qui a envoie la commande
+ */
 void    parse_cmd(std::string args, IrcServer *IRC, user currentUser)
 {
-	std::vector<std::string>	split_args = ft_split(args, " ");
-	const std::string			command = split_args.front();
-
-	IRC->recup_cmd(command)(IRC, currentUser , args);
+	std::vector<std::string>		commands;
+	std::string						args_of_commands, cmd_to_find;
+	
+	commands = ft_split(args, "\n");
+	std::string	tmp = commands.front(); //la cmd elle meme + args
+	size_t	pos = tmp.find_first_of(' '); //recuperer la position du 1e espace de notre commands
+	
+	if (pos != std::string::npos) //si cet espace ne se trouve pas a la fin de notre string = pas d'args
+	{
+		cmd_to_find = tmp.substr(0, pos); //recuperer le premier mot de la ligne
+		args_of_commands = tmp.substr(pos, tmp.length()); //stocker le reste des args
+	}
+	else 
+	{
+		cmd_to_find = tmp.substr(0, pos);
+		args_of_commands = "";
+	}
+	IRC->recup_cmd(cmd_to_find)(IRC, currentUser, args_of_commands);
 }
+
+
+
 
