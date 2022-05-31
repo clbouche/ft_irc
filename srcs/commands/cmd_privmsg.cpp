96decde0a385f92a1fe26f30ded97cceeb59f9dd
Elias AACHACH <elaachac@e3r10p12.clusters.42paris.fr>
2022-05-31 18:02:35 +0200

    notes on cmd_privmsg.cpp in order to implement privmsg

--- srcs/commands/cmd_privmsg.cpp
+++ srcs/commands/cmd_privmsg.cpp
@@ -8,7 +8,17 @@
 
 void    cmd_privmsg( IrcServer *serv, user *currentUser, std::string & args )
 {
-    (void)serv;
-    (void)currentUser;
+	(void)serv;
+	(void)currentUser;
 	(void)args;
+	// On determine d'abord si on a un seul user ou un channel
+	// si channel, on stock les users devant recevoir le message dans une stack
+	// on envoie ensuite le message dans une boucle telle que :
+	// tant que stack.size != 0:
+	//      send msg a stack.top()   -> send msg a user
+	//		stack.pop()    -> supprimer le user a qui on vient de send msg de la stack
+	//
+	// Si on a qu'un seul user : 
+	// send directement msg a user
+
 }
