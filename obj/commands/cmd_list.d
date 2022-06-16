d80fc9f2d2027837845c96c1d57bbf82e60b0df7
Elias AACHACH <elaachac@e3r9p14.clusters.42paris.fr>
2022-06-16 15:00:48 +0200

    MODE finished. maybe some format issues. need to check with irssi

---
+++ obj/commands/cmd_list.d
@@ -0,0 +1,22 @@
+obj/commands/cmd_list.o: srcs/commands/cmd_list.cpp includes/../includes/headers.hpp \
+  includes/colors.hpp includes/responses_and_errors.hpp \
+  includes/config.hpp includes/utils.hpp includes/IrcServer.hpp \
+  includes/headers.hpp includes/tcpServer.hpp includes/user.hpp \
+  includes/channels.hpp includes/../includes/commands.hpp \
+  includes/../includes/channels.hpp includes/../includes/IrcServer.hpp \
+  includes/../includes/user.hpp
+srcs/commands/cmd_list.cpp:
+includes/../includes/headers.hpp:
+includes/colors.hpp:
+includes/responses_and_errors.hpp:
+includes/config.hpp:
+includes/utils.hpp:
+includes/IrcServer.hpp:
+includes/headers.hpp:
+includes/tcpServer.hpp:
+includes/user.hpp:
+includes/channels.hpp:
+includes/../includes/commands.hpp:
+includes/../includes/channels.hpp:
+includes/../includes/IrcServer.hpp:
+includes/../includes/user.hpp:
