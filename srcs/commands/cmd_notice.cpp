869e8943d365f435400aba58e67d6140600a83b4
Elias AACHACH <elaachac@e3r9p15.42.fr>
2022-06-23 11:27:36 +0200

    fix notice, now working nice

--- srcs/commands/cmd_notice.cpp
+++ srcs/commands/cmd_notice.cpp
@@ -16,27 +16,29 @@ static bool check_args(std::string target, std::string msg_to_check)
 
 void cmd_notice(IrcServer *serv, user *currentUser, std::string &args)
 {
-	size_t pos = args.find_first_of(" ");
-	std::string target = args.substr(0, pos);
+	size_t pos = args.find_first_of(":");
+	std::string target = args.substr(0, pos - 1);
 	std::string msg_to_check;
 	pos == std::string::npos ? msg_to_check = "" : msg_to_check = args.substr(pos + 1, args.length());
 	if (check_args(target, msg_to_check))
 	{
-		std::string msg_notice = formatMsgsUsers(currentUser->getNickName(), currentUser->getUserName(), currentUser->getHostNameUser());
+		std::string msg_privmsg = formatMsgsUsers(currentUser->getNickName(), currentUser->getUserName(), currentUser->getHostNameUser());
 
 		if (strchr(CHANNEL_PREFIX, target.c_str()[0]) != NULL)
 		{
-			channels *chanToSend = serv->currentChannels.find(target)->second;
-			std::map<int, user *>::iterator it;
-			if (chanToSend->UserInChan(currentUser) == true)
+			if (serv->currentChannels.find(target) != serv->currentChannels.end())
 			{
-				for (it = chanToSend->getUsers().begin(); it != chanToSend->getUsers().end(); it++)
+				channels *chanToSend = serv->currentChannels.find(target)->second;
+				std::map<int, user *>::iterator it;
+				if (chanToSend->getName() != "" && (chanToSend->UserInChan(currentUser) == true || (chanToSend->getMode().find('i') == std::string::npos && chanToSend->UserIsBan(currentUser) == false)))
 				{
-					if (it->second->getNickName() != currentUser->getNickName())
+					for (it = chanToSend->getUsers().begin(); it != chanToSend->getUsers().end(); it++)
 					{
-					chanToSend->sendToAllUsersInChan(&serv->_tcpServer, (msg_notice + "PRIVMSG "
-							+ chanToSend->getName() + " " + target + " :"
-							+ msg_to_check + "\r\n"));
+						if (it->second->getNickName() != currentUser->getNickName())
+						{	
+							serv->_tcpServer.add_to_buffer(std::make_pair(it->second->getSdUser(), (msg_privmsg + "NOTICE "
+								+ target + " :" + msg_to_check + "\r\n"))); 
+						}
 					}
 				}
 			}
@@ -48,11 +50,12 @@ void cmd_notice(IrcServer *serv, user *currentUser, std::string &args)
 			{
 				if (it->second->getNickName() == target)
 				{
-					serv->_tcpServer.add_to_buffer(std::make_pair(it->second->getSdUser(), (msg_notice + "PRIVMSG "
-							+ target + " : " + msg_to_check + "\r\n")));
+					serv->_tcpServer.add_to_buffer(std::make_pair(it->second->getSdUser(), (msg_privmsg + "NOTICE "
+							 + msg_to_check + "\r\n")));
 					return;
 				}
 			}
 		}
 	}
 }
+
